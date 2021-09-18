18937886ff578a0d54bc85406ba26e5d98339e2c
Rinson <rinson.peter1992@gmai.com>
2021-09-18 23:52:16 +0530

    witout

--- NODE_JS/without_callback.js
+++ NODE_JS/without_callback.js
@@ -1,5 +1,4 @@
 var fs = require("fs");  
-var data = fs.readFileSync('F:\\python\\Python\\Luminar\\Python_Django\\LUMINAR\\JAVASCRIPT\\NODE_JS\\input.txt');  
+var data = fs.readFileSync('F:\\python\\Python\\Luminar\\Python_Django\\NODE_JS\\input.txt');  
 console.log(data.toString());  
 console.log("Program Ended");
-
